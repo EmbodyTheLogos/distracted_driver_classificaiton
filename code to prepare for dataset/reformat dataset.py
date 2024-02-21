98d6e10a9dcd373f1e9dd83adb3f4e82a1b7f498
Long Nguyen <43763576+EmbodyTheLogos@users.noreply.github.com>
2024-02-21 13:11:50 -0500

    Update reformat dataset.py

--- code to prepare for dataset/reformat dataset.py
+++ code to prepare for dataset/reformat dataset.py
@@ -9,6 +9,7 @@ import pickle
 DATADIR = "imgs/train"
 CATEGORIES = ["c0", "c1","c2","c3","c4","c5","c6","c7","c8","c9"]
 training_data = []
+IMG_SIZE = 224
 def create_training_data():
   for category in CATEGORIES:
     path = os.path.join(DATADIR, category) #  path to different types of drivers
@@ -17,7 +18,7 @@ def create_training_data():
     for img in os.listdir(path):
       try:
         # img_array = cv2.imread(os.path.join(path, img))
-        # new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE)) # resize to 224 x 244
+        # new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE)) # resize to 224 x 224
         img_array = Image.open(os.path.join(path, img))
         img_array = img_array.resize((IMG_SIZE, IMG_SIZE))
         img_array = np.array(img_array)
